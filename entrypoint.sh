657dbfaf79497a2f3080a357b484683a7b742363
Cássio Ribeiro Alves de Ávila <cassioavila@yandex.com>
2022-06-26 22:55:08 -0300

    add option to provide vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,10 @@
 
 input_file="${1}"
 output_file="${2}"
+if ! [ "${3+x}" ] ; then
+	vars="-D'${3}'"
+else
+	vars=""
+fi
 
-openscad -o $output_file $input_file
+openscad $vars -o $output_file $input_file
